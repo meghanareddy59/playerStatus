cdd945c558e46b536863eccb99c3fdbdf702f22c
meghana <meghana.gaddam@pramati.com>
2020-01-19 17:19:37 +0530

    playerstatusui

---
+++ playerstats.js
@@ -0,0 +1,201 @@
+var bodymain=document.createElement('div');
+bodymain.setAttribute('id','main');
+document.body.appendChild(bodymain);
+
+
+//heading
+var heading=document.createElement('div');
+heading.setAttribute('class','heading');
+heading.textContent="Players status"
+bodymain.appendChild(heading);
+
+// 1st payer
+var img1_span=document.createElement('span');
+img1_span.setAttribute('class','img');
+bodymain.appendChild(img1_span);
+
+//img
+var img1=document.createElement('img');
+img1.setAttribute('class','logo');
+img1.src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRRsH0mQIxmObLEP1RuUsCVzYUWcNOfw8XjGBSmjG23m1fnkgdTYw&s://mohitpawar.com/wp-https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRRsH0mQIxmObLEP1RuUsCVzYUWcNOfw8XjGBSmjG23m1fnkgdTYw&s/uploads/2010/10/unknown.jpg";
+img1_span.appendChild(img1);
+//details
+var setrank1=document.createElement("p")
+setrank1.textContent="Rank : 1";
+img1_span.appendChild(setrank1);
+var setname1=document.createElement("p");
+setname1.textContent="Name : Vasavi Bhanoori";
+var setteam1=document.createElement("p");
+setteam1.textContent="Team name : Sun Risers";
+var won1=document.createElement("p");
+won1.textContent="Matches won : 10";
+var lost1=document.createElement("p");
+lost1.textContent="Matches lost : 2";
+var setpoints1=document.createElement("p");
+setpoints1.textContent="Total points : 20";
+
+img1_span.appendChild(setname1);
+img1_span.appendChild(setteam1);
+img1_span.appendChild(won1);
+img1_span.appendChild(lost1);
+img1_span.appendChild(setpoints1);
+
+
+
+//2nd player
+var img2_span=document.createElement('span');
+img2_span.setAttribute('class','img');
+bodymain.appendChild(img2_span);
+
+var img2=document.createElement('img');
+img2.setAttribute('class','logo');
+img2.src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSazFMAWuMUoeU4xJU2hKckhM7AAXwltJ96bxG8wWkLgk4wbFTP&s"
+img2_span.appendChild(img2);
+var rank2=document.createElement("p")
+rank2.textContent="Rank : 2";
+img2_span.appendChild(rank2);
+
+var setname2=document.createElement("p");
+setname2.textContent="Name : Gangotri";
+var setteam2=document.createElement("p");
+setteam2.textContent="Team name : Sun Risers";
+var won2=document.createElement("p");
+won2.textContent="Matches won : 9";
+var lost2=document.createElement("p");
+lost2.textContent="Matches lost : 3";
+var setpoints2=document.createElement("p");
+setpoints2.textContent="Total points : 18";
+
+img2_span.appendChild(setname2);
+img2_span.appendChild(setteam2);
+img2_span.appendChild(won2);
+img2_span.appendChild(lost2);
+img2_span.appendChild(setpoints2);
+
+var img3_span=document.createElement('span');
+img3_span.setAttribute('class','img');
+bodymain.appendChild(img3_span);
+
+
+var img3=document.createElement('img');
+img3.setAttribute('class','logo');
+img3.src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR9abtT22pS5vCGmnIO9MTvnWPBFswpXZEm5fM4FeGDiACgAlAc&s"
+img3_span.appendChild(img3);
+//details
+var setrank3=document.createElement("p")
+setrank3.textContent="Rank : 3";
+img3_span.appendChild(setrank3);
+var setname3=document.createElement("p");
+setname3.textContent="Name : Vasavi Bhanoori";
+var setteam3=document.createElement("p");
+setteam3.textContent="Team name : Sun Risers";
+var won3=document.createElement("p");
+won3.textContent="Matches won : 6";
+var lost3=document.createElement("p");
+lost3.textContent="Matches lost : 4";
+var setpoints3=document.createElement("p");
+setpoints3.textContent="Total points : 12";
+img3_span.appendChild(setrank3);
+img3_span.appendChild(setname3);
+img3_span.appendChild(setteam3);
+img3_span.appendChild(won3);
+img3_span.appendChild(lost3);
+img3_span.appendChild(setpoints3);
+
+
+
+
+var img4_span=document.createElement('span');
+img4_span.setAttribute('class','img');
+bodymain.appendChild(img4_span);
+
+
+var img4=document.createElement('img');
+img4.setAttribute('class','logo');
+img4.src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRF_XugDIX83IHctzHiUFmdZVlsGuad8sAF6P7R5Vp01yTa4dA4&s"
+img4_span.appendChild(img4);
+var setrank4=document.createElement("p")
+setrank4.textContent="Rank : 4";
+img4_span.appendChild(setrank4);
+var setname4=document.createElement("p");
+setname4.textContent="Name : Vasavi Bhanoori";
+var setteam4=document.createElement("p");
+setteam4.textContent="Team name : Sun Risers";
+var won4=document.createElement("p");
+won4.textContent="Matches won : 5";
+var lost4=document.createElement("p");
+lost4.textContent="Matches lost : 5";
+var setpoints4=document.createElement("p");
+setpoints4.textContent="Total points : 10";
+img4_span.appendChild(setrank4);
+img4_span.appendChild(setname4);
+img4_span.appendChild(setteam4);
+img4_span.appendChild(won4);
+img4_span.appendChild(lost4);
+img4_span.appendChild(setpoints4);
+
+
+
+var img5_span=document.createElement('span');
+img5_span.setAttribute('class','img');
+bodymain.appendChild(img5_span);
+
+
+var img5=document.createElement('img');
+img5.setAttribute('class','logo');
+img5.src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRNfphC-voxtv4VMvxgh3JvtnhvqtjEYR3eC3rURI2fW6A_08IE&s"
+img5_span.appendChild(img5);
+var rank5=document.createElement("p")
+rank5.textContent="Rank : 5";
+
+
+var setrank5=document.createElement("p")
+setrank5.textContent="Rank : 5";
+img5_span.appendChild(setrank5);
+var setname5=document.createElement("p");
+setname5.textContent="Name : Vasavi Bhanoori";
+var setteam5=document.createElement("p");
+setteam5.textContent="Team name : Sun Risers";
+var won5=document.createElement("p");
+won5.textContent="Matches won : 4";
+var lost5=document.createElement("p");
+lost5.textContent="Matches lost : 6";
+var setpoints5=document.createElement("p");
+setpoints5.textContent="Total points : 8";
+img5_span.appendChild(setrank5);
+img5_span.appendChild(setname5);
+img5_span.appendChild(setteam5);
+img5_span.appendChild(won5);
+img5_span.appendChild(lost5);
+img5_span.appendChild(setpoints5);
+
+
+var img6_span=document.createElement('span');
+img6_span.setAttribute('class','img');
+bodymain.appendChild(img6_span);
+
+
+var img6=document.createElement('img');
+img6.setAttribute('class','logo');
+img6.src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQnbqVSHOd-EH6pT4VCnM1ht5LxZ2v1rZAyeMk52TOFvedOM52P&s"
+img6_span.appendChild(img6);
+var setrank6=document.createElement("p")
+setrank6.textContent="Rank : 6";
+img6_span.appendChild(setrank6);
+var setname6=document.createElement("p");
+setname6.textContent="Name : Vasavi Bhanoori";
+var setteam6=document.createElement("p");
+setteam6.textContent="Team name : Sun Risers";
+var won6=document.createElement("p");
+won6.textContent="Matches won : 3";
+var lost6=document.createElement("p");
+lost6.textContent="Matches lost : 7";
+var setpoints6=document.createElement("p");
+setpoints6.textContent="Total points : 6";
+img6_span.appendChild(setrank6);
+img6_span.appendChild(setname6);
+img6_span.appendChild(setteam6);
+img6_span.appendChild(won6);
+img6_span.appendChild(lost6);
+img6_span.appendChild(setpoints6);
+
